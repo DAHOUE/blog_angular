7a14affed520239dea19af45eda8e28102b8c859
toundji <olatoundjiagbahoungba@gmail.com>
2020-08-25 16:24:52 +0100

    ndhfd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog';
+  constructor(){
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyArgoLYF_Rdey_CKlf4SFmuFD6A9kyE6qo",
+    authDomain: "blob-7fe23.firebaseapp.com",
+    databaseURL: "https://blob-7fe23.firebaseio.com",
+    projectId: "blob-7fe23",
+    storageBucket: "blob-7fe23.appspot.com",
+    messagingSenderId: "983545156785",
+    appId: "1:983545156785:web:0a3f2f62b5652dc974fe90"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostFormComponent } from './post-list/post-form/post-form.component'
+import { PostService } from './services/post.service';
+import { CanActivate, RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PostListItemComponent } from './post-list/post-list-item/post-list-item.component';
 
+const appRoutes: Routes=[
+  { path: 'posts', component: PostListComponent},
+  { path: 'form', component: PostFormComponent},
+  { path: '', redirectTo: 'posts', pathMatch: 'full'},
+  { path: '**', redirectTo: 'posts'}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostFormComponent,
+    HeaderComponent,
+    PostListItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    PostService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
